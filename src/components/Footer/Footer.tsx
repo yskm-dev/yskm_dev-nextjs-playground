8c6366ca87949b4a505f99b541935e97f974046c
yskm-dev <yskm.dev.0818@gmail.com>
2026-01-30 16:05:48 +0900

    feat: フッターのリンクをNext.jsに更新し、リンク定数を整理
    - Footerコンポーネントで使用するフレームワークのリンクをNext.jsに変更
    - links.tsでのリンク定数を整形し、Next.jsのリンクを追加

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,42 +1,51 @@
 'use client';
-import { Nav } from "@/components/Nav";
-import { EXTERNAL_LINKS } from "@/constants/links";
-import IconArrow from "@/svg/icon_arrow.svg";
-import styles from "./Footer.module.scss";
+import { Nav } from '@/components/Nav';
+import { EXTERNAL_LINKS } from '@/constants/links';
+import IconArrow from '@/svg/icon_arrow.svg';
+import styles from './Footer.module.scss';
 
 export function Footer() {
   return (
-  <footer className={styles.footer}>
-  <div className={styles.footer__inner}>
-    <div className={styles.stack}>
-      <h2 className={styles.stack__title}>Built with:</h2>
-      <ul>
-        <li>
-          Framework: <a href={EXTERNAL_LINKS.astro} target="_blank">Astro<IconArrow aria-hidden="true" /></a>
-        </li>
-        <li>
-          Programming Language: <a
-            href={EXTERNAL_LINKS.typescript}
-            target="_blank"
-            >TypeScript
-            <IconArrow aria-hidden="true" /></a>
-        </li>
-        <li>
-          CMS: <a href={EXTERNAL_LINKS.microCMS} target="_blank"
-            >MicroCMS<IconArrow aria-hidden="true" /></a>
-        </li>
-        <li>
-          Hosting Platform: <a href={EXTERNAL_LINKS.cloudflare} target="_blank"
-            >Cloudflare<IconArrow aria-hidden="true" /></a>
-        </li>
-      </ul>
-    </div>
-    <div className={styles.links}>
-      <Nav label="フッターナビゲーション" isHeader={false} />
-    </div>
-  </div>
-  <p className={styles.copyright}>© 2026 yskm_dev</p>
-</footer>
-
-  )
+    <footer className={styles.footer}>
+      <div className={styles.footer__inner}>
+        <div className={styles.stack}>
+          <h2 className={styles.stack__title}>Built with:</h2>
+          <ul>
+            <li>
+              Framework:{' '}
+              <a href={EXTERNAL_LINKS.nextjs} target="_blank">
+                Next.js
+                <IconArrow aria-hidden="true" />
+              </a>
+            </li>
+            <li>
+              Programming Language:{' '}
+              <a href={EXTERNAL_LINKS.typescript} target="_blank">
+                TypeScript
+                <IconArrow aria-hidden="true" />
+              </a>
+            </li>
+            <li>
+              CMS:{' '}
+              <a href={EXTERNAL_LINKS.microCMS} target="_blank">
+                MicroCMS
+                <IconArrow aria-hidden="true" />
+              </a>
+            </li>
+            <li>
+              Hosting Platform:{' '}
+              <a href={EXTERNAL_LINKS.cloudflare} target="_blank">
+                Cloudflare
+                <IconArrow aria-hidden="true" />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.links}>
+          <Nav label="フッターナビゲーション" isHeader={false} />
+        </div>
+      </div>
+      <p className={styles.copyright}>© 2026 yskm_dev</p>
+    </footer>
+  );
 }
--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,8 +1,9 @@
 export const EXTERNAL_LINKS = {
-  x: "https://twitter.com/_yskm_dev_",
-  github: "https://github.com/yskm-dev",
-  astro: "https://astro.build/",
-  typescript: "https://www.typescriptlang.org/",
-  microCMS: "https://microcms.io/",
-  cloudflare: "https://pages.cloudflare.com/",
+  x: 'https://twitter.com/_yskm_dev_',
+  github: 'https://github.com/yskm-dev',
+  astro: 'https://astro.build/',
+  nextjs: 'https://nextjs.org/',
+  typescript: 'https://www.typescriptlang.org/',
+  microCMS: 'https://microcms.io/',
+  cloudflare: 'https://pages.cloudflare.com/',
 };
